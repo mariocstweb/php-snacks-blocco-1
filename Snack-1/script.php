96efb43ff131c08008cb667d443b1c485658120d
mariocstweb <mariocstmra@gmail.com>
2024-02-19 14:33:30 +0100

    created array for snack1

---
+++ Snack-1/script.php
@@ -0,0 +1,24 @@
+<?php
+
+$matchs = [
+  "match1" => [
+    "casa" => "Milano",
+    "ospite" => "Torino",
+    "punti_casa" => 55,
+    "punti_ospite" => 60,
+  ],
+  "match2" => [
+    "casa" => "Firenze",
+    "ospite" => "Bologna",
+    "punti_casa" => 30,
+    "punti_ospite" => 65,
+  ],
+  "match3" => [
+    "casa" => "Roma",
+    "ospite" => "Lazio",
+    "punti_casa" => 10,
+    "punti_ospite" => 70,
+  ],
+];
+
+?>
