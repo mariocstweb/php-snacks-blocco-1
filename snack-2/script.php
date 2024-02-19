0326b20964cf01166fd5e835635d081a4686b849
mariocstweb <mariocstmra@gmail.com>
2024-02-19 15:26:28 +0100

    added script.php snack2

---
+++ snack-2/script.php
@@ -0,0 +1,33 @@
+<?php
+// Recupero i valori in GET
+$name = $_GET['name'];
+$email = $_GET['email'];
+$age = $_GET['age']; 
+
+// Flag di partenza
+$access_allowed = true;
+
+// Controllo se il nome ha almeno tre caratteri
+if (strlen($name) < 3) {
+  $access_allowed = false;
+  echo "Nome non valido, inserisci almeno tre caratteri.";
+}
+
+// Controllo se sono presenti questi caratteri nell'email: "@" e "."
+if (strpos($email, '@') === false || strpos($email, '.') === false) {
+  $access_allowed = false;
+  echo "Formato email non valido.";
+}
+
+// Controllo se l'età è un numero
+if (!is_numeric($age)) {
+  $access_allowed = false;
+  echo "L'età inserita non è un numero.";
+}
+
+if ($access_allowed) {
+  echo "Accesso riuscito.";
+} else {
+  echo "Accesso negato.";
+}
+?>
